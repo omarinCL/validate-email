5bba4a0ea784d41688519f37490ddad46e5f0eb5
Orlando Marin <orlando.marin@outlook.cl>
2020-02-12 23:42:59 -0300

    CORS

--- src/main/java/cl/option/validateemail/controller/EmailValidateController.java
+++ src/main/java/cl/option/validateemail/controller/EmailValidateController.java
@@ -5,8 +5,10 @@ import javax.validation.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,7 @@ import cl.option.validateemail.service.EmailValidation;
 @RestController
 @RequestMapping("/api/v1/email")
 @Validated
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET}) 
 public class EmailValidateController {
     @Autowired
     private EmailValidation service;
